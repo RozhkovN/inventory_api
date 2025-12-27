f7c3e665f4a2443532e18ff491cf0b8c8355c9b1
Mighty9862 <admin@admin.local>
2025-12-27 11:22:16 +0300

    first commit

--- init.sql
+++ init.sql
@@ -5,15 +5,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- База данных уже будет создана благодаря POSTGRES_DB,
--- но мы гарантируем её существование
-DO $$ 
-BEGIN 
-  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'inventory_db') THEN 
-    CREATE DATABASE inventory_db;
-  END IF;
-END 
-$$;
-
--- Убедиться что пользователь имеет доступ
+-- Гарантировать права доступа
 GRANT ALL PRIVILEGES ON DATABASE inventory_db TO inventory_user;
